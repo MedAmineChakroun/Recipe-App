556dcf640017512d67aff53459a2629caf2aee3b
MedAmineChakroun <164560880+MedAmineChakroun@users.noreply.github.com>
2025-01-21 15:48:05 +0100

    fix paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import Search from "./components/Search";
-import FoodList from "./components/foodList";
-import Nav from "./components/Nav";
+import FoodList from "./components/foodList.jsx";
+import Nav from "./components/Nav.jsx";
 import styles from "./App.module.css";
-import Container from "./components/Container";
-import InnerContainer from "./components/InnerContainer";
-import FoodDetails from "./components/FoodDetails";
-import ApiRequests from "./components/ApiRequests";
+import Container from "./components/Container.jsx";
+import InnerContainer from "./components/InnerContainer.jsx";
+import FoodDetails from "./components/FoodDetails.jsx";
 function App() {
   const [foods, setFoods] = useState([]);
   const [foodId, setFoodId] = useState("656329");
